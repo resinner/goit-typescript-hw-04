e81bb1617780d0ca879acb0bbb750e4541985533
resinner <revengingsinner@gmail.com>
2023-11-07 17:53:30 +0200

    Task3 done

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,13 @@
-// import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
-// export function FormComponent() {
-//   const [value, setValue] = useState("");
+export function FormComponent() {
+  const [value, setValue] = useState<string>("");
 
-//   const handleChange = (event) => {
-//     setValue(event.target.value);
-//   };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
 
-//   return (
-//     <input type="text" value={value} onChange={handleChange} />
-//   );
-// }
+  return (
+    <input type="text" value={value} onChange={handleChange} />
+  );
+}
