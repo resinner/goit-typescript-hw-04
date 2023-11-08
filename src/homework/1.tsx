ec75621b136b728aa42306759b70e94ff60ecf18
resinner <revengingsinner@gmail.com>
2023-11-08 13:47:41 +0200

    change options type

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -6,11 +6,11 @@ interface Props {
   onContentEndVisible: () => void;
 }
 
-type Options = {
-      rootMargin: string,
-      threshold: number,
-      root: null | Element,
-    };
+// type Options = {
+//       rootMargin: string,
+//       threshold: number,
+//       root: null | Element,
+//     };
 
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
@@ -18,7 +18,7 @@ export function Observer({ children, onContentEndVisible }: Props) {
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options: Options = {
+    const options: IntersectionObserverInit = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
